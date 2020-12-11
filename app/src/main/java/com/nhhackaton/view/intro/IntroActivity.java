cc9dc6423bc3191c6e23930e800c6d152a0e5c24
hongyeongjune <hongyeongjune@naver.com>
2020-12-11 16:14:28 +0900

    인트로 화면 작성

---
+++ app/src/main/java/com/nhhackaton/view/intro/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.nhhackaton.view.intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nhhackaton.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
