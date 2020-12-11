a4e88cec7b31eeff218309b84dd7c4217fc61f5d
hongyeongjune <hongyeongjune@naver.com>
2020-12-11 16:15:12 +0900

    사용자 로그인 화면 작성

---
+++ app/src/main/java/com/nhhackaton/view/signin/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.nhhackaton.view.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nhhackaton.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
