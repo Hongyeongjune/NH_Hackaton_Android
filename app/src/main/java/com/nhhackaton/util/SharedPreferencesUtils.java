34ea1d3a6327116c3eb2629b513037a9a18bbdfc
hongyeongjune <hongyeongjune@naver.com>
2020-12-12 17:10:35 +0900

    자동 로그인 구현

--- app/src/main/java/com/nhhackaton/util/SharedPreferencesUtils.java
+++ app/src/main/java/com/nhhackaton/util/SharedPreferencesUtils.java
@@ -22,4 +22,16 @@ public class SharedPreferencesUtils {
         return sharedPreferences.getString("MEMBER_EMAIL", null);
 
     }
+
+    public static void writeAutoSignInTo(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("NH_HACKATON", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("MEMBER_AUTO_SIGN_IN", true);
+        editor.apply();
+    }
+
+    public static boolean readAutoSignInFromToken(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("NH_HACKATON", Context.MODE_PRIVATE);
+        return sharedPreferences.getBoolean("MEMBER_AUTO_SIGN_IN", false);
+    }
 }
