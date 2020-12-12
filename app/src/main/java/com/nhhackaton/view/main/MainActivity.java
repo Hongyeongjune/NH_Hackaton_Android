ed702141e6728cfe750c41b74296f911b90a1ad1
hongyeongjune <hongyeongjune@naver.com>
2020-12-12 19:48:34 +0900

    네비게이션 뷰 화면 전환 프레그먼트로 수정

--- app/src/main/java/com/nhhackaton/view/main/MainActivity.java
+++ app/src/main/java/com/nhhackaton/view/main/MainActivity.java
@@ -9,32 +9,21 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 import com.nhhackaton.R;
-import com.nhhackaton.data.DepositMoney.source.DepositMoneyRepository;
-import com.nhhackaton.util.SharedPreferencesUtils;
-import com.nhhackaton.view.main.presenter.MainContract;
-import com.nhhackaton.view.main.presenter.MainPresenter;
+import com.nhhackaton.view.home.HomeFragment;
+import com.nhhackaton.view.investhistory.InvestHistoryFragment;
+import com.nhhackaton.view.loanhistory.LoanHistoryFragment;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, MainContract.View {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Context context;
     private Toolbar toolbar;
     private ImageView ivSideMenu;
     private DrawerLayout dlSideMenu;
     private NavigationView nvSideMenu;
-
-    private TextView tvDepositMoney;
-    private Button btnInvest;
-    private Button btnInvestHistory;
-    private Button btnLoan;
-    private Button btnLoanHistory;
-
-    private MainContract.Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,29 +44,47 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         ivSideMenu.setOnClickListener(v -> dlSideMenu.openDrawer(GravityCompat.START));
 
-        tvDepositMoney = (TextView) findViewById(R.id.tv_main_deposit_money);
-        btnInvest = (Button) findViewById(R.id.btn_main_invest);
-        btnInvestHistory = (Button) findViewById(R.id.btn_main_invest_history);
-        btnLoan = (Button) findViewById(R.id.btn_main_loan);
-        btnLoanHistory = (Button) findViewById(R.id.btn_main_loan_history);
-
-        presenter = new MainPresenter(this, DepositMoneyRepository.getInstance());
-        presenter.callReadDepositMoney(SharedPreferencesUtils.readMemberFromEmail(context));
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.ll_main, HomeFragment.createFragment())
+                .commit();
+        invalidateOptionsMenu();
 
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.drawer_menu_home:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.ll_main, HomeFragment.createFragment())
+                        .commit();
+                invalidateOptionsMenu();
+                dlSideMenu.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.drawer_menu_information:
+                break;
+            case R.id.drawer_menu_invest:
+                break;
+            case R.id.drawer_menu_invest_history:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.ll_main, InvestHistoryFragment.createFragment())
+                        .commit();
+                invalidateOptionsMenu();
+                dlSideMenu.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.drawer_menu_loan:
+                break;
+            case R.id.drawer_menu_loan_history:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.ll_main, LoanHistoryFragment.createFragment())
+                        .commit();
+                invalidateOptionsMenu();
+                dlSideMenu.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.drawer_menu_logout:
+                break;
+        }
+
         return false;
-    }
-
-    @Override
-    public void showErrorMessage(String message) {
-
-    }
-
-    @Override
-    public void setDepositMoney(String message) {
-        tvDepositMoney.setText(message + "원");
     }
 }
